706cf99622cba1f77dcdf42ef2cbc3e9667d04aa
Matt Hambro <matthew.hambro@gmail.com>
2022-12-07 17:37:15 -0800

    day 07 savepoint, fixing foldersize compute

--- day_07/day_07.go
+++ day_07/day_07.go
@@ -5,14 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"os"
-
-	// "sort"
 	"strconv"
 	"strings"
 )
 
 func GetTestData(filename string) (data []string, err error) {
-	fmt.Println("In get test data")
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println(err)
@@ -26,7 +23,7 @@ func GetTestData(filename string) (data []string, err error) {
 	for scanner.Scan() {
 		ret = append(ret, scanner.Text())
 	}
-	return ret, nil
+	return ret, scanner.Err()
 }
 
 func ParseTestData(data []string) (map[string][]string, map[string]int) {
@@ -43,7 +40,6 @@ out:
 			folderName := fields[2]
 			path = append(path, folderName)
 			pathString := strings.Join(path, ".")
-			fmt.Println("path string", pathString)
 
 			for j := i + 2; !strings.HasPrefix(data[j], "$") && j < len(data); j++ {
 				fields := strings.Fields(data[j])
@@ -63,112 +59,74 @@ out:
 				if j == len(data)-1 {
 					break out
 				}
+				i = j
 			}
 		}
 		if strings.HasPrefix(line, "$ cd") && strings.HasSuffix(line, "..") {
 			// pop last dir name from path
 			path = path[:len(path)-1]
-			fmt.Println("trimmed path to", path)
 		}
 	}
-	// fmt.Println(folderSizes)
-	// fmt.Println(folderTree)
 
 	return folderTree, folderSizes
 }
 
-func GetSizeOfFolders(folderTree map[string][]string, folderSizes map[string]int) int {
-	queue := make([]string, 0)
-	queue = append(queue, "/") // add start to queue
+func GetSizeOfFolders(folderTree map[string][]string, folderSizes map[string]int) (p1 int, p2 int) {
+	fmt.Println("root size", folderSizes["/"])
 
-	parents := make(map[string]string)
-	parents["/"] = ""
+	 
 
-	// for len(queue) > 0 {
-	// 	node := queue[0]
-	// 	queue = queue[1:]
-	// 	for _, v := range folderTree[node] {
-	// 		if _, ok := parents[v]; !ok {
-	// 			// add enclosing folder as parent
-	// 			parents[v] = node
-
-	// 			// add current folder to queue
-	// 			queue = append(queue, v)
-	// 		}
-	// 		if _, ok := folderTree[v]; !ok {
-	// 			fmt.Println("end found, update all parents of", v)
-	// 			fmt.Println("end folder has size:", folderSizes[v])
-	// 			prev := v
-	// 			for node, ok := parents[v]; ok && node != ""; node, ok = parents[node] {
-	// 				fmt.Println()
-	// 				fmt.Println("updating folder", node)
-	// 				fmt.Println("current folder size", folderSizes[node])
-	// 				fmt.Println("with child folder", v)
-	// 				fmt.Println("child folder size", folderSizes[prev])
-	// 				folderSizes[node] += folderSizes[prev]
-	// 				fmt.Println("new current folder size", folderSizes[node])
-	// 				// fmt.Println("press enter to continue")
-	// 				// input := bufio.NewScanner(os.Stdin)
-	// 				// input.Scan()
-	// 				prev = node
-	// 			}
-	// 		}
-	// 	}
-	// }
-	for folderString := range folderSizes {
-		fmt.Println("working on folderString", folderString)
-		folders := strings.Split(folderString, ".")
-		for i := len(folders); i > 1; i-- {
-			folder := strings.Join(folders[:i], ".")
-			size := folderSizes[folder]
-			fmt.Println("folder", folder)
-			fmt.Println("size", size)
-			parent := strings.Join(folders[:i-1], ".")
-
-			// add current folder size to parent
-			folderSizes[parent] += folderSizes[folder]
+	for name := range folderSizes {
+		if _, ok := folderTree[name]; !ok {
+			fmt.Println("end found", name, folderSizes[name])
 			fmt.Println()
+			folders := strings.Split(name, ".")
+			for i := len(folders); i > 1; i-- {
+				folder := strings.Join(folders[:i], ".")
+				parent := strings.Join(folders[:i-1], ".")
+				fmt.Println("updating parent of", folder)
+				fmt.Println("folder has", folderSizes[folder])
+				fmt.Println("parent has", folderSizes[parent])
+				// add current folder size to parent
+				folderSizes[parent] += folderSizes[folder]
+				fmt.Println("parent new value", folderSizes[parent])
+			}
+		} else {
+			fmt.Println("skippin")
 		}
 	}
-	// fmt.Println(folderSizes)
+	fmt.Println("root size", folderSizes["/"])
 
-	sum := 0
+	// p1 answer
 	for _, val := range folderSizes {
 		if val < 100000 {
-			sum += val
+			p1 += val
 		}
 	}
-	// foldersBySize := make([]string, 0, len(folderSizes))
 
-	// fmt.Println(foldersBySize)
+	fmt.Println("root size", folderSizes["/"])
+
+	// p2 answer
+	// foldersBySize := make([]string, 0, len(folderSizes))
 	// for name := range folderSizes {
 	// 	foldersBySize = append(foldersBySize, name)
 	// }
-	// fmt.Println("\n")
-	// fmt.Println(folderSizes)
 
 	// sort.SliceStable(foldersBySize, func(i, j int) bool {
-	// 	return folderSizes[foldersBySize[i]] > folderSizes[foldersBySize[j]]
+	// 	return folderSizes[foldersBySize[i]] < folderSizes[foldersBySize[j]]
 	// })
+
+	// for name := range folderSizes {
+	// 	fmt.Println(name, folderSizes[name])
+	// }
 
 	// for _, name := range foldersBySize {
 	// 	originalSize := folderSizes["/"]
-	// 	fmt.Println()
-	// 	fmt.Println("size of folder", folderSizes[name])
-	// 	fmt.Println("new size", originalSize-folderSizes[name])
+	// 	newSize := originalSize - folderSizes[name]
+	// 	if 70000000-newSize > 30000000 {
+	// 		p2 = folderSizes[name]
+	// 		break
+	// 	}
 	// }
-	// fmt.Println(folderSizes)
-	return sum
-
-}
-
-func main() {
-	testInput, _ := GetTestData("large_input.txt")
-	fmt.Println(testInput, testInput)
-	tree, sizes := ParseTestData(testInput)
-	res := GetSizeOfFolders(tree, sizes)
-
-	fmt.Println("Answer")
-	fmt.Println(res)
-	// not 1609610
+	return p1, p2
 }
--- day_07/day_07_test.go
+++ day_07/day_07_test.go
@@ -1,18 +1,48 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestDay07(t *testing.T) {
 
-	testInput, _ := GetTestData("large_input.txt")
-	// fmt.Println(testInput)
-	tree, sizes := parseTestData(testInput)
-	res := getSizeOfFolders(tree, sizes)
+	testSmallInput, err := GetTestData("small_input.txt")
+	if err != nil {
+		t.Error("error reading file", err)
+	}
+	tree, sizes := ParseTestData(testSmallInput)
+	p1Got, p2Got := GetSizeOfFolders(tree, sizes)
 
-	fmt.Println("Answer")
-	fmt.Println(res)
+	p1Want := 95437
+	if p1Got != p1Want {
+		t.Errorf("got %v, want %v", p1Got, p1Want)
+	}
+
+	p2Want := 24933642
+
+	if p2Got != p2Want {
+		t.Errorf("got %v, want %v", p2Got, p2Want)
+	}
+
+	testBigInput, errBig := GetTestData("large_input.txt")
+	if errBig != nil {
+		t.Error("error reading file", err)
+	}
+	treeBig, sizesBig := ParseTestData(testBigInput)
+	p1GotBig, p2GotBig := GetSizeOfFolders(treeBig, sizesBig)
+
+	p1WantBig := 1611443
+	if p1GotBig != p1WantBig {
+		t.Errorf("got %v, want %v", p1GotBig, p1WantBig)
+	}
+
+	p2WantBig := -1
+	maxFileSystem := 70000000
+	if p2GotBig > maxFileSystem {
+		t.Errorf("got %v, want < %v", p2GotBig, maxFileSystem)
+	}
+	if p2GotBig != p1WantBig {
+		t.Errorf("got %v, want %v", p2GotBig, p2WantBig)
+	}
+
 }
-
