bf20f60258d1c5c9c6a5b108a2530788d97805b4
Matt Hambro <matthew.hambro@gmail.com>
2022-12-13 21:00:24 -0800

    Save day 13

--- day_13/day_13.go
+++ day_13/day_13.go
@@ -32,30 +32,32 @@ func PacketsInOrder(leftSlice []interface{}, rightSlice []interface{}, debug boo
 	for lI, rI := 0, 0; lI < len(leftSlice) && rI < len(leftSlice); lI, rI = lI+1, rI+1 {
 		left, right := leftSlice[lI], rightSlice[rI]
 		leftType, rightType := reflect.TypeOf(left).Kind(), reflect.TypeOf(right).Kind()
-
+		DebugPrint(debug, "left %v", left)
+		DebugPrint(debug, "right %v", right)
 		switch {
 		case leftType == reflect.Float64 && rightType == reflect.Float64 && left.(float64) > right.(float64):
+			DebugPrint(debug, "both int")
 			return false
 		case leftType == reflect.Slice && rightType == reflect.Slice:
+			DebugPrint(debug, "both slice")
+			// TODO() check indexes and lengths
 			return PacketsInOrder(left.([]interface{}), right.([]interface{}), debug)
 		case leftType != rightType:
+			DebugPrint(debug, "one int, one slice")
 			if leftType == reflect.Float64 {
 				return PacketsInOrder([]interface{}{left}, right.([]interface{}), debug)
 			} else {
-				DebugPrint(debug, "left %v %T", left, left)
-				DebugPrint(debug, "right %v %T", right, right)
 				return PacketsInOrder(left.([]interface{}), []interface{}{right}, debug)
 			}
 		}
 
-		// TODO() check indexes and lengths
 	}
 
 	return true
 }
 
 func SumPacketsInOrder(packets []string, debug bool) (sumRightOrderIndices int) {
-	for i := 0; i < len(packets); i += 2 {
+	for i := 0; i < len(packets); i += 3 {
 		p1 := packets[i]
 		p2 := packets[i+1]
 		index := ((i + 2) / 3) + 1
@@ -64,9 +66,9 @@ func SumPacketsInOrder(packets []string, debug bool) (sumRightOrderIndices int) 
 		json.Unmarshal([]byte(p1), &left)
 		json.Unmarshal([]byte(p2), &right)
 
-		DebugPrint(debug, "pair %v", index)
+		DebugPrint(debug, "pair %v ++++++++++", index)
 		inOrder := PacketsInOrder(left, right, debug)
-		DebugPrint(debug, "in order %v", inOrder)
+		DebugPrint(debug, "in order %v ++++++++++", inOrder)
 
 		if inOrder {
 			sumRightOrderIndices += index
